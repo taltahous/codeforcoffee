e2ff2a75acf496109d6d5774878cd0d7107e4535
taltahous <taibaaltahous@gmail.com>
2017-05-31 21:18:06 +0300

    admincrude

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 from django.contrib.auth import login
-from views import createCoffee
-from
 from .views import *
 
 urlpatterns = [
 
    url(r'^$', main, name="home"),
-    url(r'^createcoffee', createCoffee, name='createCoffee'),
+    url(r'^createcoffee/$', createCoffee, name='createCoffee'),
     url(r'^editcoffee/(?P<coffee_id>[0-9]+)/$', editCoffee, name='editCoffee'),
+    url(r'^editbean/(?P<bean_id>[0-9]+)/$', editBean, name='editBean'),
+    url(r'^createbean/$', createBean, name='createBean'),
+    url(r'^deletecoffee/(?P<coffee_id>[0-9]+)/$', deleteCoffee, name='deleteCoffee'),
+    url(r'^deletebean/(?P<bean_id>[0-9]+)/$', deleteBean, name='deleteBean'),
 
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -9,43 +9,87 @@ from .forms import *
 def main(request):
     return render(request, "main.html", {})
 
-    def login(request):
-        return render(request, "login.html", {})
 
-        def createCoffee(request):
-            context = {}
-            if request.method == "POST":
-                form = CoffeeForm(request.POST)
-                context['form'] = form
-                if form.is_valid():
-                    coffee = form.save(commit=False)
-                    coffee.user = request.user
-                    coffee.save()
-                    form.save_m2m()
+def login(request):
+    return render(request, "login.html", {})
+
+
+def createCoffee(request):
+    context = {}
+    if request.method == "POST":
+        form = CoffeeForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            coffee = form.save(commit=False)
+            coffee.user = request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("home")
+        else:
+            return render(request,"createCoffee.html", context)
+    else:
+        form = CoffeeForm()
+        context['form'] = form
+        return render(request, "createCoffee.html", context)
+
+
+def editCoffee(request, coffee_id):
+    context = {}
+    coffee = Coffee.objects.get(id=coffee_id)
+    context['coffee'] = coffee
+    if request.method == "POST":
+        form = CoffeeForm(request.POST, instance=coffee)
+        context['form'] = form
+        if form.is_valid():
+            coffee = form.save(commit=False)
+            coffee.user = request.user
+            coffee.save
+            form.save_m2m()
+            return redirect("home")
+        else:
+            return render(request, "editCoffee.html", context)
+    else:
+        form = CoffeeForm(instance=coffee)
+        context['form'] = form
+        return render(request, "editCoffee.html", context)
+
+
+def createBean(request):
+    context = {}
+    if request.method == "POST":
+        form = BeanForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            coffee = form.save()
+            return redirect("home")
+        else:
+            return render(request, "createCoffee.html", context)
+    else:
+        form = BeanForm()
+        context['form'] = form
+        return render(request, "createCoffee.html", context)
+
+def editBean(request, bean_id):
+        context = {}
+        bean = Bean.objects.get(id=bean_id)
+        context['bean'] = bean
+        if request.method == "POST":
+            form = BeanForm(request.POST, instance=bean)
+            context['form'] = form
+            if form.is_valid():
+                bean = form.save()
                 return redirect("home")
             else:
-                return render(request,“createCoffee.html”, context)
-                else:
-                    form = CoffeeForm()
-                    context['form'] = form
-                     return render(request,"createCoffee.html", context)
+                return render(request, "editBean.html", context)
+        else:
+                form = BeanForm(instance=bean)
+                context['form'] = form
+                return render(request, "editBean.html", context)
 
-                def editCoffee(request, coffee_id):
-                    context = {}
-                    coffee = Coffee.objects.get(id=coffee_id)
-                    context['coffee']=coffee
-                    if request.method == "POST":
-                        form = CoffeeForm(request.POST, instance=coffee)
-                        context['form']=form
-                        if form.is_valid():
-                            coffee = form.save(commit=False)
-                            coffee.user = request.user
-                            coffee.save
-                            form.save_m2m()
-                            return redirect("home")
-                        else:
-                            return render(request,"editCoffee.html", context)
-                        else:
-                            form = CoffeeForm(instance=coffee)
-                            context['form']=form
-                            return render(request,"editCoffee.html", context)
+def deleteBean(request, bean_id):
+    Bean.objects.get(id=bean_id).delete()
+    return redirect("home")
+
+def deleteCoffee(request, coffee_id):
+    Coffee.objects.get(id=coffee_id).delete()
+    return redirect("home")
